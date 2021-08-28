861de4c9abb6d50aea92e4b4d8496f13d431620d
Pandabro-07 <89648666+Pandabro-07@users.noreply.github.com>
2021-08-28 15:06:16 +0530

    Update Activity_04.py

--- Activity_04.py
+++ Activity_04.py
@@ -1,4 +1,6 @@
 num1=int(input())
 num2=int(input())
 num3=num1 + num2
-print("%d + %d = %d\n"%(num1,num2,num3))
+print("%d + %d = %d"%(num1,num2,num3))
+print("{} + {} = {}".format(num1,num2,num3))
+print(f'{num1} + {num2} = {num3}')
